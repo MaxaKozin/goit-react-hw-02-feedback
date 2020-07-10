543cfdf65b1b32d5693707b8f442cb062eda4608
Maxim Kozin <maxa.kozin@gmail.com>
2020-07-10 16:50:26 +0300

    Updates

---
+++ precache-manifest.74d2dbae42e5c797ecb3869212b78898.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd83811314d397fabd4677acca33043",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "056934dc27ccd7783ac8",
+    "url": "/goit-react-hw-02-feedback/static/css/main.4082f69c.chunk.css"
+  },
+  {
+    "revision": "34f0c2ba6114eb0d74d2",
+    "url": "/goit-react-hw-02-feedback/static/js/2.6444399b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-02-feedback/static/js/2.6444399b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "056934dc27ccd7783ac8",
+    "url": "/goit-react-hw-02-feedback/static/js/main.52b4018d.chunk.js"
+  },
+  {
+    "revision": "cb08e2ec47267fd4ee9f",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.9cb7e66e.js"
+  }
+]);
