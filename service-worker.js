551c6ce05cf9b16524e5ea6addd919b6e31039b2
Maxim Kozin <maxa.kozin@gmail.com>
2020-09-01 19:58:08 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-02-feedback/precache-manifest.8187eb342bde9b70923f062b5c7a5baa.js"
+  "/goit-react-hw-02-feedback/precache-manifest.c49c6cabaea4eb1184a46c45253b8f73.js"
 );
 
 self.addEventListener('message', (event) => {
