89e7b1e6338f18ee458585f602e50733eadf6ed6
Maxim Kozin <maxa.kozin@gmail.com>
2020-09-02 11:49:05 +0300

    formatting using prettier

--- src/App.js
+++ src/App.js
@@ -8,42 +8,46 @@ class App extends Component {
   state = {
     good: 0,
     neutral: 0,
-    bad: 0
-  }
+    bad: 0,
+  };
 
   countTotalFeedback = () => {
     const { good, bad, neutral } = this.state;
     return good + neutral + bad;
-  }
+  };
 
   countPositiveFeedbackPercentage = () => {
     const { good } = this.state;
     const total = this.countTotalFeedback();
     if (total) {
       return `${Math.round((good / total) * 100)}%`;
-    };
+    }
     return '0%';
-  }
+  };
 
-
-  incrementHandler = (e) => {
-    const key = e.target.name
-    this.setState(prev => (
-      { [key]: prev[key] + 1 })
-    )
-  }
+  incrementHandler = e => {
+    const key = e.target.name;
+    this.setState(prev => ({ [key]: prev[key] + 1 }));
+  };
 
   render() {
     return (
       <>
         <Section title="Please leave feedback">
-          <FeedbackOptions options={['good', 'neutral', 'bad']} onIncrement={this.incrementHandler} />
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onIncrement={this.incrementHandler}
+          />
         </Section>
         <Section title="Statistics">
-          <Statistics {...this.state} total={this.countTotalFeedback()} positivePercentage={this.countPositiveFeedbackPercentage()} />
+          <Statistics
+            {...this.state}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
         </Section>
       </>
-    )
+    );
   }
 }
 
